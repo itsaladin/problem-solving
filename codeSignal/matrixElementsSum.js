bc9ee57b1f6a3c221a53bd0ae66801542602f335
aladdin <alauddin12340@gmail.com>
2022-10-22 22:45:58 +0600

    solve matrixElementsSum problem

---
+++ codeSignal/matrixElementsSum.js
@@ -0,0 +1,12 @@
+function solution(matrix) {
+    let sum = 0;
+    for(let i = 0; i<matrix[0].length; i++){
+        for(let j = 0; j<matrix.length; j++){
+            if(matrix[j][i] == 0){
+                break;
+            }
+            sum += matrix[j][i];
+        }
+    }
+    return sum;
+}
