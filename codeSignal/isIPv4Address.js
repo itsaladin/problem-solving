f85e5b98777ece99d65b8516ef45bf911978da8e
aladdin <alauddin12340@gmail.com>
2022-10-24 23:28:18 +0600

    solve isIPv4Address problem

---
+++ codeSignal/isIPv4Address.js
@@ -0,0 +1,9 @@
+function solution(inputString) {
+    const octet = '(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)';
+    const regex = new RegExp(`^${octet}\\.${octet}\\.${octet}\\.${octet}$`);
+  
+      if(regex.test(inputString)){
+        return true;
+      }
+      return false;
+  }
