94f8c730d3080a18ab73bc6fe93cd99ac882d584
aladdin <alauddin12340@gmail.com>
2022-10-27 23:58:44 +0600

    solve Fizz buzz problem

---
+++ leetcode/Fizz Buzz.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+ var fizzBuzz = function(n) {
+    let i = 1;
+    let result = []
+    while(i <= n){
+        if(i%3==0 && i%5==0){
+         result[i-1] = "FizzBuzz"
+        }
+        else if(i%3==0){
+          result[i-1] = 'Fizz'
+        }
+        else if(i%5==0){
+         result[i-1] = "Buzz"
+        }
+        else{ 
+            result[i-1] = `${i}`
+        }
+        
+        i++
+    }
+    return result;
+};
