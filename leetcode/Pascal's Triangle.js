dc0abb22e1719821904952b992ee65b3aea30b1b
aladdin <alauddin12340@gmail.com>
2022-11-11 22:59:29 +0600

    sovle Pascal's Triangle problem

---
+++ leetcode/Pascal's Triangle.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+ var generate = function(numRows) {
+    let arr = []
+    for(i=0; i < numRows; i++){
+        arr[i] = [];
+        arr[i][0] = 1;
+        for(j=1; j < i; j++){
+            arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
+        }
+        arr[i][i] = 1;
+    }
+    return arr;
+};
